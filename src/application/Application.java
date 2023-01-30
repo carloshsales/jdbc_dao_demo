061396e485032dbb98addee7954ffff8e061af43
Carlos Henrique <carlosh.sales@outlook.com>
2023-01-30 20:10:44 -0300

    create: seller class

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,10 +1,22 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Department depart = new Department(1, "IT");
+		System.out.println(depart);
+		
+		System.out.println();
+		
+		Seller seller = new Seller(1, "Carlos", "carlos@gmail.com", new Date(), 1200.00, depart);
+		System.out.println(seller);
+		
 	}
 
 }
